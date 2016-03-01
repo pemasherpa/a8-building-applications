5eebb31cc31c686f06ef1bc1d855552b15858335
Pema <sherpa.pems@gmail.com>
2016-03-01 12:45:01 -0800

    added server/ui

---
+++ iris/server.R
@@ -0,0 +1,13 @@
+library(shiny)
+
+shinyServer(function(input, outpit)) {
+  
+  output$distPlot <- renderPlot({
+    
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    hist(x, breaks = bins, col = "darkgray", border = "white")
+    
+  })
+}
